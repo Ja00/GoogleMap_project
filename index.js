4c00dc5f146db6323201c39c92a6e4f82fe8cd19
root <root@ip-172-31-42-159.us-east-2.compute.internal>
2017-09-10 01:32:59 +0000

    This is a failed version, trying to saperate the css and js from index.html

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+const path = require('path');
+const express = require('express');
+const app = express();
 
-const PORT_NUMBER = 8080
+const PORT_NUMBER = 8080;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/templates/mymap.html'));
-})
+});
+
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
 
 app.listen(PORT_NUMBER, function () {
   console.log('Example app listening on port ' + PORT_NUMBER + '!')
