f5a4e7b3fd7fe482298441f59d1ca8cdd9c5fa1c
root <root@ip-172-31-42-159.us-east-2.compute.internal>
2017-09-15 04:57:38 +0000

    This is the first commit for Class_3

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+const path = require('path')
+const express = require('express')
+const app = express()
+const request = require('request')
 
-const PORT_NUMBER = 8080;
+const PORT_NUMBER = 8080
+const NEARBY_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+const GOOGLE_API_KEY = 'AIzaSyBZ8PZCoEFJzI7-3PEFcz_-n3PNJO8KRcw';
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/templates/mymap.html'));
 });
 
-app.use('/css', express.static(path.join(__dirname, 'css')));
-app.use('/js', express.static(path.join(__dirname, 'js')));
+
+
+/*app.get('/nearby_search', function (req, api_res) {
+  var radius = req.query.radius || 150;
+  var params = {
+    'key': GOOGLE_API_KEY,
+    'location': req.query.location,
+    'radius': radius,
+    'type': req.query.type
+  };
+  request({url: NEARBY_SEARCH_URL, qs: params}, function(err, res, body) {
+    if( res.statusCode == 200) {
+      api_res.json(body);
+    } else {
+    //error helper
+    }
+  });
+});*/
+ 
+
+app.use('/css', express.static('css'))
+app.use('/js', express.static('js'))
 
 app.listen(PORT_NUMBER, function () {
   console.log('Example app listening on port ' + PORT_NUMBER + '!')
-})
+});
--- js/mymap.js
+++ js/mymap.js
@@ -1,11 +1,66 @@
+var DEFAULT_ZOOM = 15;
+// var GOOGLE_API_KEY = 'AIzaSyBZ8PZCoEFJzI7-3PEFcz_-n3PNJO8KRcw';
+var service; 
+var map;
+var DEFAULT_RADIUS = 1500;
+var DEFAULT_LAT = 37.773872;
+var DEFARULT_LNG = -122.43129;
+
+$(function() {
 function initMap() {
-   var uluru = {lat: -25.363, lng: 131.044};
-   var map = new google.maps.Map(document.getElementById('map'), {
-     zoom: 4,
-     center: uluru
+ 
+ var input = document.getElementById('pac-input');
+ var searchBox = new google.maps.places.SearchBox(input);  
+  
+ map = new google.maps.Map($('#map')[0], {
+     zoom: DEFAULT_ZOOM,
+     center: {
+      lat: DEFAULT_LAT,
+      lng: DEFAULT_LNG
+     }
    });
-   var marker = new google.maps.Marker({
-     position: uluru,
-     map: map
-   });
- }
+ map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); 
+ map.addListener('counds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+ });
+  location: new google.maps.Latlng(DEFAULT_LAT, DEFAULT_LNG),
+  radius: DEFAULT_RADIUS,
+  type: 'restaurant'
+};
+
+service = new google.maps.places.PlacesService(map);
+service.textSearch(request, callback);  
+
+
+initMap();
+
+function callback(results, status) {
+  if(status === google.maps.places.PlacesServiceStatus.OK) {
+    var current_infowindow;
+    _.each(results, function(place){
+      var marker = new google.maps.Marker({
+        position: {
+          'lat': place.geometry.location.lat(),
+          'lng': place.geometry.location.lng()
+        },
+        map:map
+      });
+      
+      var infowindow_content = 
+          '<div id="content"> + '<h1 id="firstHeading">' + place.name + '</h1>'+
+          '</div>';
+      var infowindow = new google.maps.InfoWindow({
+          content: infowindow_content
+      });
+
+      marker.addListener('click', function() {
+        if (current_infowindow) {
+          current_infowindow.close();
+         }
+        infowindow.open(map, marker);
+        current_infowindow = infowindow;
+      });
+    });
+  }    
+});
+}
